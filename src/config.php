5d62f837c73364c9fd726a07aeee406f2f44b1d6
Tom.Huang <hzlhu.dargon@gmail.com>
2014-10-30 18:34:04 +0800

    complete config logic

--- src/config.php
+++ src/config.php
@@ -1,9 +1,40 @@
 <?php
 namespace Assets;
 
-class Config
+final class Config
 {
-    public static function init()
+    private static $_serverRootPath;
+
+    private static $_jsDirectoryPath;
+    private static $_cssDirectoryPath;
+
+    private static $_compiledJsDirectoryPath;
+    private static $_compiledCssDirectoryPath;
+
+
+    public static function init($serverRootPath,
+        $jsDirectoryPath, $cssDirectoryPath,
+        $compiledJsDirectoryPath, $compiledCssDirectoryPath)
     {
+        self::$_serverRootPath = realpath($serverRootPath);
+
+        self::$_jsDirectoryPath = realpath($jsDirectoryPath);
+        self::$_cssDirectoryPath = realpath($cssDirectoryPath);
+
+
+        if (file_exists($compiledJsDirectoryPath) === false) {
+            mkdir($compiledJsDirectoryPath, 0770, true);
+        }
+        if (file_exists($compiledCssDirectoryPath) === false) {
+            mkdir($compiledCssDirectoryPath, 0770, true);
+        }
+        self::$_compiledJsDirectoryPath = realpath($compiledJsDirectoryPath);
+        self::$_compiledCssDirectoryPath = realpath($compiledCssDirectoryPath);
+    }
+
+    public static function __callStatic($method, $arguments)
+    {
+        $property = '_' . lcfirst(str_replace('get', '', $method));
+        return self::$$property;
     }
 }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,2 +1,8 @@
 <?php
-Assets\Config::init();
+Assets\Config::init(
+    __DIR__ . '/app',
+    __DIR__ . '/app/assets/javascripts',
+    __DIR__ . '/app/assets/stylesheets',
+    __DIR__ . '/app/tmp/assets/javascripts',
+    __DIR__ . '/app/tmp/assets/stylesheets'
+);
