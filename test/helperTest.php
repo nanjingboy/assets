8b1200cd22a7edae5a10cf9ef423094f12ed1281
Tom.Huang <hzlhu.dargon@gmail.com>
2014-11-06 11:46:03 +0800

    remove unnecessary test

--- test/helperTest.php
+++ test/helperTest.php
@@ -1,4 +1,5 @@
 <?php
+use Assets\Config;
 use Assets\Helper;
 
 class HelperTest extends PHPUnit_Framework_TestCase
@@ -23,43 +24,24 @@ class HelperTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    public function testImageUrl()
+    public function testAssetUrl()
     {
+        $baseDir = Config::getImagesPath();
         $this->assertEquals(
             'http://somehost.com/image.png',
-            Helper::imageUrl('http://somehost.com/image.png')
+            Helper::assetUrl('http://somehost.com/image.png', $baseDir)
         );
         $this->assertEquals(
             'https://somehost.com/image.png',
-            Helper::imageUrl('https://somehost.com/image.png')
+            Helper::assetUrl('https://somehost.com/image.png', $baseDir)
         );
         $this->assertEquals(
             "data:image/png:base64,0",
-            Helper::imageUrl('data:image/png:base64,0')
+            Helper::assetUrl('data:image/png:base64,0', $baseDir)
         );
         $this->assertEquals(
             '/assets/images/banner.png',
-            Helper::imageUrl('banner.png')
-        );
-    }
-
-    public function testFontUrl()
-    {
-        $this->assertEquals(
-            'http://somehost.com/font.eot',
-            Helper::imageUrl('http://somehost.com/font.eot')
-        );
-        $this->assertEquals(
-            'https://somehost.com/font.eot',
-            Helper::imageUrl('https://somehost.com/font.eot')
-        );
-        $this->assertEquals(
-            "data:application/font-eot:base64,0",
-            Helper::imageUrl('data:application/font-eot:base64,0')
-        );
-        $this->assertEquals(
-            '/assets/fonts/font.eot',
-            Helper::fontUrl('font.eot')
+            Helper::assetUrl('banner.png', $baseDir)
         );
     }
 }
