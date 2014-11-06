2a44a42ef29efc9849bce83973d99638f27297ab
Tom.Huang <hzlhu.dargon@gmail.com>
2014-11-06 10:14:37 +0800

    fix coffee compile permission denied error via browser

--- lib/compiler/coffee.php
+++ lib/compiler/coffee.php
@@ -9,16 +9,8 @@ class Coffee extends AbstractCompiler
 
     protected static function _compile($srcFile, $distFile)
     {
-        /**
-         * As CoffeeScript cli can't set a different file path with compile command,
-         * so we have to compile the file in src directory path, and then move to the dist path
-         */
-        $command = 'coffee --compile --no-header --bare ' . $srcFile->getPathname();
-        if (Shell::run($command) === true) {
-            return rename(
-                rtrim($srcFile->getPathname(), $srcFile->getExtension()) . 'js',
-                $distFile->getPathname()
-            );
+        if (copy($srcFile->getPathname(), $distFile->getPathname())) {
+            return Shell::run('coffee --compile --no-header --bare ' . $distFile->getPathname());
         }
 
         return false;
