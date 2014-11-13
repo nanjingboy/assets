f9d2426654e1928c2c0d8f7d55689313807f6869
Tom.Huang <hzlhu.dargon@gmail.com>
2014-11-13 08:54:05 +0800

    add prefix argument

--- assets.php
+++ assets.php
@@ -1,23 +1,25 @@
 <?php
 use Assets\Helper;
 
-function javascript_include_tag($file)
+function javascript_include_tag($file, $prefix = null)
 {
     $includes = array();
     $files = Helper::includeAsset($file, 'js');
+    $prefix = rtrim($prefix, DIRECTORY_SEPARATOR);
     foreach ($files as $file) {
-        array_push($includes, "<script src='{$file}'></script>");
+        array_push($includes, "<script src='{$prefix}{$file}'></script>");
     }
 
     return $includes;
 }
 
-function stylesheet_include_tag($file)
+function stylesheet_include_tag($file, $prefix = null)
 {
     $includes = array();
     $files = Helper::includeAsset($file, 'css');
+    $prefix = rtrim($prefix, DIRECTORY_SEPARATOR);
     foreach ($files as $file) {
-        array_push($includes, "<link href='{$file}' media='screen' rel='stylesheet'/>");
+        array_push($includes, "<link href='{$prefix}{$file}' media='screen' rel='stylesheet'/>");
     }
 
     return $includes;
